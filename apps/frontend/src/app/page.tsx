150c4bda4680a5ff9d4c180cceac115e8f1e6528
zoritto <zoritto@zoritto.zoritto>
2024-11-20 14:40:49 -0300

    ✨ Intriduzindo o ID
    introduzindo o componente ID, no projeto

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,8 +1,10 @@
 import Pagina from "@/components/template/pagina";
+import { Evento } from "@/core";
 
 export default function Home() {
+  const e: Partial <Evento> = {};
   return (
-    <Pagina>
+    <Pagina className="w-full">
       <div>Inicio</div>
     </Pagina>
   );
